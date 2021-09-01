a36ccf5f0c4ac5ce0922fe6f580e16ea31f4bfd1
Harderthan <kheo1772@gmail.com>
2021-09-01 22:13:07 +0900

    앞서 소개한 연습문제 구조 구현

--- Chapter01/main.cpp
+++ Chapter01/main.cpp
@@ -1,31 +1,86 @@
-#include <string>
 #include <algorithm>
 #include <iostream>
-using namespace std;
+#include <list>
 
-template<typename T> class dynamic_array
-{
-    T *data;
-    size_t n;
+struct CircularllNode {
+  std::string data;
+  CircularllNode *next;
+};
+using node = CircularllNode;
+using node_ptr = node *;
+struct CircularllIterator {
+private:
+  node_ptr ptr;
 
 public:
-    dynamic_array(int n)
-    {
-        this->n = n;
-        data = new T[n];
-    }
-    dynamic_array(const dynamic_array<t> &other)
-    {
-        n = other.n;
-        data = new T[n];
+  CircularllIterator(node_ptr p) : ptr(p) {}
+  std::string &operator*() { return ptr->data; }
+  node_ptr get() { return ptr; }
+  CircularllIterator &operator++() {
+    ptr = ptr->next;
+    return *this;
+  }
+  CircularllIterator operator++(int) {
+    CircularllIterator result = *this;
+    ++(*this);
+    return result;
+  }
+  friend bool operator==(const CircularllIterator &left,
+                         const CircularllIterator &right) {
+    return left.ptr == right.ptr;
+  }
+  friend bool operator!=(const CircularllIterator &left,
+                         const CircularllIterator &right) {
+    return left.ptr != right.ptr;
+  }
+};
+class Circularll {
+private:
+  node_ptr head;
 
-        for (int i = 0; i < n; i++)
-            data[i] = other[i];
+public:
+  Circularll() = default;
+  Circularll(const Circularll &other) : head(NULL) {
+    if (other.head) {
+      head = new node{0, NULL};
+      auto cur = head;
+      auto it = other.begin();
+      while (true) {
+        cur->data = *it;
+
+        auto tmp = it;
+        ++tmp;
+        if (tmp == other.end())
+          break;
+
+        cur->next = new node{0, NULL};
+        cur = cur->next;
+        it = tmp;
+      }
     }
+  }
+  Circularll(const std::initializer_list<std::string> &ilist) : head(NULL) {
+    for (auto it = std::rbegin(ilist); it != std::rend(ilist); it++)
+      push_front(*it);
+  }
+  void push_front(std::string val) {
+    auto new_node = new node{val, NULL};
+    if (head != NULL)
+      new_node->next = head;
+    head = new_node;
+  }
+  void pop_front() {
+    auto first = head;
+    if (head) {
+      head = head->next;
+      delete first;
+    }
+  }
+  
+  CircularllIterator begin() { return CircularllIterator(head); }
+  CircularllIterator end() { return CircularllIterator(NULL); }
+  CircularllIterator begin() const { return CircularllIterator(head); }
+  CircularllIterator end() const { return CircularllIterator(NULL); }
 };
 
-int main(int argc, char **argv)
-{
-    cout << "hello, world" << endl;
-    return 0;
-}
+int main(int argc, char **argv) { return 0; }
